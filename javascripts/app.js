bd8340042ff187484ec4d75d727682c4c70a02e3
Mike Nieva <miguelnieva@conexionew.com>
2018-04-14 22:35:15 -0500

    Prework

---
+++ javascripts/app.js
@@ -0,0 +1,162 @@
+// Rover Object Goes Here
+// ======================
+let rover = {
+    direction: "N",
+    fullDirection: "North",
+    x: 0,
+    y: 0,
+    travelLog: []
+}
+
+
+console.log("Welcome to the game");
+console.log("You are going to drive a Rover on the space. 🚀 ");
+
+// Functions
+// ======================
+function turnLeft(rover){
+  switch (rover.direction){
+    case "N":
+      rover.direction = "W";
+      rover.fullDirection = "West";
+      break;
+    case "W":
+      rover.direction = "S";
+      rover.fullDirection = "South";
+      break;
+    case "S":
+      rover.direction = "E";
+      rover.fullDirection = "East";
+      break;
+    case "E":
+      rover.direction = "N";
+      rover.fullDirection = "North";
+      break;
+  }
+  console.log("Rover is now facing " + rover.fullDirection);
+  console.log("=====");
+}
+
+function turnRight(rover){
+  switch (rover.direction){
+    case "N":
+      rover.direction = "E";
+      rover.fullDirection = "East";
+      break;
+    case "E":
+      rover.direction = "S";
+      rover.fullDirection = "South";
+      break;
+    case "S":
+      rover.direction = "W";
+      rover.fullDirection = "West";
+      break;
+    case "W":
+      rover.direction = "N";
+      rover.fullDirection = "North";
+      break;
+  }
+  console.log("Rover is now facing " + rover.fullDirection); 
+  console.log("=====");
+}
+
+function moveForward(rover){
+  switch (rover.direction) {
+    case "W":
+      if (rover.x == 0){
+        console.log("Turn left or right. The rover could be roam off the map!");
+      } else {
+        rover.x -= 1;
+      }
+      break;
+
+    case "N":
+        if (rover.y == 0){
+          console.log("Turn left or right. The rover could be roam off the map!");
+        } else {
+          rover.y -= 1;
+        }
+      break;
+
+    case "S":
+        if (rover.y == 10){
+          console.log("Turn left or right. The rover could be roam off the map!");
+        } else {
+          rover.y += 1;
+        }
+      break;
+
+    case "E":
+      if (rover.x == 10){
+        console.log("Turn left or right. The rover could be roam off the map!");
+      } else {
+        rover.x += 1;
+      }
+      break;                      
+  }
+  console.log("The Rover's coordinates are: " + rover.x + "," + rover.y + " and it's pointing to the " + rover.fullDirection);
+  console.log("=====");
+  getLog(rover);
+}
+
+function moveBackward(rover){
+  switch (rover.direction) {
+    case "W":
+      if (rover.x > 10){
+        console.log("Turn left or right. The rover could be roam off the map!");
+      } else {
+        rover.x += 1;
+      }
+      break;
+
+    case "N":
+        if (rover.y > 10){
+          console.log("Turn left or right. The rover could be roam off the map!");
+        } else {
+          rover.y += 1;
+        }
+      break;
+
+    case "S":
+        if (rover.y == 0){
+          console.log("Turn left or right. The rover could be roam off the map!");
+        } else {
+          rover.y -= 1;
+        }
+      break;
+
+    case "E":
+      if (rover.x == 0){
+        console.log("Turn left or right. The rover could be roam off the map!");
+      } else {
+        rover.x -= 1;
+      }
+      break;                      
+  }  
+  console.log("The Rover's coordinates are: " + rover.x + "," + rover.y + " and it's pointing to the " + rover.fullDirection);
+  console.log("=====");
+  getLog(rover);
+}
+
+function commands(letters) {
+  for (i=0; i<letters.length ;i++){
+    switch (letters.charAt(i)){
+      case "f":
+        moveForward(rover);
+        break;
+      case "r":
+        turnRight(rover);
+        break;
+      case "l":
+        turnLeft(rover);        
+        break; 
+      case "b":
+        moveBackward(rover);
+        break;   
+    }
+  }
+}
+
+function getLog(rover){
+    rover.travelLog.push("(" + rover.x + ", " + rover.y +")" );
+}
